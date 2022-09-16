0477a212b9178456f7af9b160a96f889a47c4282
dgmaikon <dgmaikon@hotmail.com>
2022-09-16 19:31:31 -0300

    Create compose route.

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ app.get("/about", function(req, res) {
 
 });
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+  
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
